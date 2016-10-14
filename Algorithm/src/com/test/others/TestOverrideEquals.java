51493867c3e2457fbdabbcfe94a0d80df31228a4
Jonathan Zhou <zhouronghui30@163.com>
2016-10-15 00:03:12 +0800

    test override equals methos

--- Algorithm/src/com/test/others/TestOverrideEquals.java
+++ Algorithm/src/com/test/others/TestOverrideEquals.java
@@ -1,11 +1,15 @@
 package com.test.others;
 
 public class TestOverrideEquals {
-    
+
     public static void main(String[] args) {
-        
+        TestOverrideEqualsDto dto1 = new TestOverrideEqualsDto();
+        dto1.setUserID("001");
+        dto1.setUserName("zhou rh");
+        TestOverrideEqualsDto dto2 = new TestOverrideEqualsDto();
+        dto2.setUserID("001");
+        dto2.setUserName("zhou rh");
+        System.out.println(dto1.equals(dto2));
     }
-    
-    
-    
+
 }
