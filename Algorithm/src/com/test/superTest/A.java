743ae559b640fd2bcddada2deb896a76c4154c8d
Jonathan Zhou <zhouronghui30@163.com>
2016-11-13 22:22:13 +0800

    Test for 在java中父类调用子类中的方法

---
+++ Algorithm/src/com/test/superTest/A.java
@@ -0,0 +1,18 @@
+
+package com.test.superTest;
+
+public class A implements ab {
+
+    @Override
+    public void initData() {
+        B b = new B();
+        b.initData();
+    }
+
+    @Override
+    public void Data() {
+        B b = new B();
+        b.Data();
+    }
+
+}
