534202e9d13ee64a47c17b18413cd4fdbbec5289
Jonathan Zhou <zhouronghui30@163.com>
2017-02-12 22:52:16 +0800

    Test for Spring AOP

---
+++ Algorithm/src/main/java/com/test/aop/PersonServer.java
@@ -0,0 +1,9 @@
+package com.test.aop;
+
+/**
+ * Created by IBM on 2017/2/12.
+ */
+public interface PersonServer {
+
+    public void start(String name);
+}
---
+++ Algorithm/src/main/java/com/test/aop/impl/PersonServerImpl.java
@@ -0,0 +1,15 @@
+package com.test.aop.impl;
+
+import com.test.aop.PersonServer;
+
+/**
+ * Created by IBM on 2017/2/12.
+ */
+public class PersonServerImpl implements PersonServer {
+
+    @Override
+    public void start(String name) {
+        // TODO Auto-generated method stub
+
+    }
+}
