c49683ffa2ecc58d3348b6f49c9a32b1bde68898
Jonathan <zhouronghui30@163.com>
2017-02-23 17:16:41 +0800

    test for string intern

--- Algorithm/src/main/java/com/test/helloworld/HelloWorld.java
+++ Algorithm/src/main/java/com/test/helloworld/HelloWorld.java
@@ -13,9 +13,11 @@ public class HelloWorld {
 
     private static void testStringIntern() {
 
-        String str1 = new StringBuffer("Computer").append("software").toString();
-        System.out.println(str1 == str1);
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern() == str1);
 
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);
     }
 
 }
