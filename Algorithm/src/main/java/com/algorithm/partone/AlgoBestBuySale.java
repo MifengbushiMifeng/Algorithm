9b6c7c975d820bf19b166ae9e9038914105a0c4c
Jonathan Zhou <zhouronghui30@163.com>
2017-03-30 23:07:58 +0800

    A review of best buy and sale method

--- Algorithm/src/main/java/com/algorithm/partone/AlgoBestBuySale.java
+++ Algorithm/src/main/java/com/algorithm/partone/AlgoBestBuySale.java
@@ -4,22 +4,21 @@ public class AlgoBestBuySale {
 
     public static void main(String[] args) {
 
-        int[] price = { 4, 3, 7, 1, 6, 5, 2 };
+        int[] price = {4, 3, 7, 1, 6, 5, 2};
         getBestBS(price);
     }
 
     private static void getBestBS(int[] price) {
 
-        if (price == null || price.length == 0) {
-            return;
-        }
+        if (price == null || price.length == 0) return;
 
         int min = price[0];
-        int buyDay = 1;
         int profit = 0;
+        int buyDay = 1;
         int saleDay = 1;
 
         for (int i = 1; i < price.length; i++) {
+
             if (min > price[i]) {
                 min = price[i];
                 buyDay = i;
@@ -29,12 +28,12 @@ public class AlgoBestBuySale {
                     saleDay = i;
                 }
             }
+
         }
 
-        System.out.println("profit = " + profit);
-        System.out.println("Buy day = " + (buyDay + 1));
-        System.out.println("Sale day = " + (saleDay + 1));
-
+        System.out.println("the profit is " + profit);
+        System.out.println("the best buy day is " + (buyDay + 1));
+        System.out.println("the best sale day is " + (saleDay + 1));
     }
 
 }
