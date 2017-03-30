dcfbfd6748945a6cfb86c7496b8130e9ffada9e6
Jonathan Zhou <zhouronghui30@163.com>
2017-03-30 22:52:35 +0800

    A review of Ball method

--- Algorithm/src/main/java/com/algorithm/partone/Ball.java
+++ Algorithm/src/main/java/com/algorithm/partone/Ball.java
@@ -1,26 +1,26 @@
 package com.algorithm.partone;
 
+/**
+ * A ball drop from 100m, and will jump to the 50% of the origin height
+ * to get the sum distance after 10 times.
+ */
 public class Ball {
 
-    /**
-     * Jonathan ZHou
-     * @param args
-     */
+
     public static void main(String[] args) {
-        // Testing
+
         double h = 100 / 2;
         double s = 100;
+
         for (int i = 0; i < 9; i++) {
-
-            s = s + h * 2;
+            s = s + h / 2;
             h = h / 2;
-
         }
-        System.out.println(s + " / " + h);
+
+        System.out.println(s);
+        System.out.println(h);
+
     }
 
-    private boolean checkDifferent(String inString) {
-        return !inString.matches(".*(.)");
-    }
 
 }
