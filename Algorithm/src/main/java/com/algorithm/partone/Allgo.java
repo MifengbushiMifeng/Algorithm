8a25d6c91261e43204edee4783976601f66dfe39
Jonathan Zhou <zhouronghui30@163.com>
2017-04-01 23:20:07 +0800

    allgi commit

---
+++ Algorithm/src/main/java/com/algorithm/partone/Allgo.java
@@ -0,0 +1,22 @@
+package com.algorithm.partone;
+
+/**
+ * Created by Jonathan on 2017/4/1.
+ * This class / file is used for...
+ */
+public class Allgo {
+
+    public static void main(String[] args) {
+
+
+        // int tmp = 2;
+
+        for (int i = 2; i < 30; i = i + 2) {
+
+
+        }
+
+
+    }
+
+}
