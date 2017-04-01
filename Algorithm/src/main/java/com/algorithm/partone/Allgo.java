cc2d3aa76332df2cccc408d029d0d515e06fd374
Jonathan Zhou <zhouronghui30@163.com>
2017-04-01 23:42:51 +0800

    implement of allgo

--- Algorithm/src/main/java/com/algorithm/partone/Allgo.java
+++ Algorithm/src/main/java/com/algorithm/partone/Allgo.java
@@ -8,11 +8,20 @@ public class Allgo {
 
     public static void main(String[] args) {
 
+        
+        for (int i = 2; i < 100; i = i + 2) {
 
-        // int tmp = 2;
+            int tmp = 0;
 
-        for (int i = 2; i < 30; i = i + 2) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    tmp = tmp + j;
+                }
+            }
 
+            if (tmp > i) {
+                System.out.println(i);
+            }
 
         }
 
